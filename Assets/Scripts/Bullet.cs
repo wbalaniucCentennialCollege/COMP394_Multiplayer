185da9f0a29530f006c8661e3f8f43440b023ecb
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-11-12 15:31:42 -0500

    Health Bar

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        var hit = other.gameObject;
+        var health = hit.GetComponent<Health>();
+        if(health != null)
+        {
+            health.TakeDamage(10);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
